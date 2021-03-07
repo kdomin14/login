c87d47579ad92638f587ffddf11290ec4d4f784d
Kayla <avila.kayla4@gmail.com>
2021-03-07 06:38:55 -0600

    Final Product

---
+++ src/router.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+import Home from './views/Home.vue'
+import Secret from './views/Secret.vue'
+import Login from './views/Login.vue'
+import NotFound from './views/NotFound.vue'
+
+import {isAuthenticated} from './helpers/useAuth'
+
+const routes = [
+    {path: '/', component: Home }, {path: '/secret', component: Secret, 
+    beforeEnter: (to,from,next) => {
+        console.log(isAuthenticated.value)
+        if (isAuthenticated.value) return true 
+        return '/login'
+    }, 
+    },
+
+    {path: '/login', component: Login },
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
+]
+
+export const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
